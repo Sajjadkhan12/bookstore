c57ac17eb08cfb793aceaa3c4b624bb1aaf8caae
Sajjadkhan12 <sajjadkhan12@users.noreply.github.com>
2020-06-04 12:35:20 -0700

    dd

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -121,14 +121,10 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 
 
