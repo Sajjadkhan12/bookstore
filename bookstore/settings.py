15889e022a60c6ebb74d2627ecebe8cb936556a7
Sajjadkhan12 <sajjadkhan12@users.noreply.github.com>
2020-06-04 22:20:51 -0700

    setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = '$(@kyv*7hkrfmo1p5ea&(ix05_xx7p(paz6bxrgqo-se#q9vt('
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mydjbookstore.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,26 +112,25 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url
-
-prod_db = dj_database_url.config(conn_max_age=200)
-DATABASES['default'].update(prod_db)
 
 
 
@@ -154,5 +153,4 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 
-SECURE_SSL_REDIRECT= True
-CSRF_COOKIE_SECURE = True
+
